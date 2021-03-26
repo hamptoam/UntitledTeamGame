dfeba1352856715100eb2c5a94241b14fcb62b94
Amelia Hampton <hamptoam.dev@outlook.com>
2021-03-26 00:13:17 -0500

    Init commit of files
    player object and controller implemented with basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerRb.velocity = transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerRb.velocity = -transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerRb.velocity = transform.right * speed;
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerRb.velocity = -transform.right * speed;
+        } 
+    }
+}
