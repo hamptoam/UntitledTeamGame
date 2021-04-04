2128fb1b82b12a669d7e5049fb1430cd62646e5b
Amelia Hampton <hamptoam.dev@outlook.com>
2021-04-04 00:18:16 -0500

    Updated to LTS, fixed PlayerController
    Fixed jumping issue, organized textures into Materials folder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0.4f;
-    public Rigidbody PlayerRb;
-    public bool PlayerIsOnTheGround = false;
-    public Vector3 jump;
-    public float jumpForce = 2.0f;
-    // Start is called before the first frame update
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    public float speed = 0.4f; 
+
+    private Rigidbody playerRb;
+
+    
     private void Start()
-    {
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
-        PlayerRb = GetComponent<Rigidbody>();
+    { 
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
-    //movement
-    void Update()
+    void FixedUpdate()
     {
-        float xDirection = Input.GetAxis("Horizontal"); //left, right
-        float zDirection = Input.GetAxis("Vertical"); //Back, forward. In unity z is up and down
+        float xDirection = Input.GetAxis("Horizontal"); 
+        float zDirection = Input.GetAxis("Vertical"); 
         Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
         transform.position += moveDirection * speed;
-    }
-    //Jump
-    private void FixedUpdate()
-    {
-        if (Input.GetKey(KeyCode.Space) && PlayerIsOnTheGround)
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
-            PlayerIsOnTheGround = false;
-            PlayerRb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
+
     private void OnCollisionStay(Collision collisionInfo)
     {
         if (collisionInfo.gameObject.tag == "Ground")
         {
-            PlayerIsOnTheGround = true;
+            isOnGround = true;
         }
     }
 }
